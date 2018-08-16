a1a0b04f42883961d9100e4d6c32863edbfbf4bd
Raaj108 <rajj108@outlook.com>
2018-08-16 16:57:38 -0700

    forcast view

--- modules/forecast/forcast.service.js
+++ modules/forecast/forcast.service.js
@@ -1,35 +1,40 @@
 weatherApp.factory('forecastService', ['$resource', function ($resource) {
-    var services = {};
+  var services = {};
 
-    services.find = function (city) {
-        var baseURL = "http://api.openweathermap.org/data/2.5/forecast";
+  services.find = function (city) {
+    var baseURL = "http://api.openweathermap.org/data/2.5/forecast";
 
-        var weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast", {
-            callback: "JSON_CALLBACK"
-        }, {
-            get: {
-                method: "JSONP"
-            }
-        });
+    var weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast", {
+      callback: "JSON_CALLBACK"
+    }, {
+      get: {
+        method: "JSONP"
+      }
+    });
 
-        var weatherResult = weatherAPI.get({
-            q: city,
-            appid: "b7b34c59bf0046797f7135fbce4d68f8"
-        }).$promise;
+    var weatherResult = weatherAPI.get({
+      q: city,
+      appid: "b7b34c59bf0046797f7135fbce4d68f8"
+    }).$promise;
 
-        return weatherResult;
-    }
+    return weatherResult;
+  }
 
-    services.getDate = function (date) {
-        return new Date(date * 1000);
-    }
+  services.getDate = function (date) {
+    return new Date(date * 1000);
+  }
 
-    services.weatherInfo = function (result) {
-        return result.list[0].weather[0];
-    }
+  services.weatherInfo = function (result) {
+    return result.list[0].weather[0];
+  }
 
-    services.tempInfo = function (result) {
-        return result.list[0].main;
-    }
-    return services;
+  services.tempInfo = function (result) {
+    return result.list[0].main;
+  }
+
+  services.windInfo = function (result) {
+    return result.list[0].wind;
+  }
+
+  return services;
 }]);
--- modules/forecast/forecast.controller.js
+++ modules/forecast/forecast.controller.js
@@ -1,19 +1,44 @@
 weatherApp.controller('forecastCtrl', ['forecastService', function (forecastService) {
-    var vm = this;
-    vm.city = "vadodara";
-    vm.results = {
-        city: '',
-        weather: ''
-    };
-    forecastService.find(vm.city)
-        .then(function (result) {
-            vm.results.city = result.city;
-            vm.results.weather = forecastService.weatherInfo(result);
-            vm.results.temperature = forecastService.tempInfo(result);
-            //console.log(result)
-        }, function (error) {
-            vm.errors = error;
-        });
-    console.log(vm.results);
+  var vm = this;
+  vm.city = "vadodara";
+  vm.results = {
+    city: '',
+    weather: ''
+  };
+  forecastService.find(vm.city)
+    .then(function (result) {
+      vm.results.city = result.city;
+      vm.results.weather = forecastService.weatherInfo(result);
+      vm.results.temperature = forecastService.tempInfo(result);
+      vm.results.wind = forecastService.windInfo(result);
+      console.log(result)
+    }, function (error) {
+      vm.errors = error;
+    });
+  //console.log(vm.results);
+
+  vm.changeUnit = function (unit) {
+    if (unit === 'f') {
+      vm.fahrenheit = true;
+    } else {
+      vm.fahrenheit = false;
+    }
+  }
+
+  vm.convertToFahrenheit = function (degk) {
+    return Math.round(1.8 * (degk - 273) + 32);
+  }
+
+  vm.convertToCelcius = function (degk) {
+    return Math.round(degk - 273.15);
+  }
+
+  vm.convertDate = function (date) {
+    return new Date(date * 1000);
+  }
+  
+  vm.convertKM = function (speed) {
+    return Math.round(speed * 1.609);
+  }
 
 }]);
